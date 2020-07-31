680c1bf62b7f8f02a79115bcb8a3bbc8e42d0aae
NadeeshWijesingha <nadeeshwijesingha@gmail.com>
2020-07-31 11:24:51 +0530

    Complete CrudUtil using varietyArgs

--- src/dao/CrudUtil.java
+++ src/dao/CrudUtil.java
@@ -4,14 +4,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 
 import db.DBConnection;
 
 public class CrudUtil {
 
-  public static boolean executeUpdate(){
-    return false;
+  public static boolean executeUpdate(String sql , Object... params) throws SQLException {
+    Connection connection = DBConnection.getInstance().getConnection();
+    PreparedStatement pstm = connection.prepareStatement(sql);
+    int i = 0;
+    for (Object param : params) {
+      i++;
+      pstm.setObject(i, param);
+    }
+    return pstm.executeUpdate() > 0;
   }
 
   public static ResultSet executeQuery(String sql , Object... params) throws SQLException {
